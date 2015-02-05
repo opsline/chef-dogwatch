73d977080e849031dac9fa8b948880c38a1ec2a5
Radek Wierzbicki <radek@opsline.com>
2015-02-05 12:48:40 -0500

    use version attribute; readme skeleton

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,9 +2,21 @@
 # Cookbook Name:: opsline-dogwatch
 # Recipe:: default
 #
-# Copyright (C) 2015 YOUR_NAME
+# Author:: Radek Wierzbicki
 #
-# All rights reserved - Do Not Redistribute
+# Copyright 2014, OpsLine, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 include_recipe 'python'
@@ -26,7 +38,7 @@ user node['opsline-dogwatch']['user'] do
 end
 
 remote_file '/usr/local/bin/dogwatch.py' do
-  source 'https://raw.githubusercontent.com/opsline/dogwatch/0.1.1/dogwatch.py'
+  source "https://raw.githubusercontent.com/opsline/dogwatch/#{node['opsline-dogwatch']['version']}/dogwatch.py"
   owner 'root'
   group 'root'
   mode 0755
